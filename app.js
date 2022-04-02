a28a4f07db384c5dbb0cc078785fd30c013cfbeb
Mayara S Carvalho <79992764+bregadero@users.noreply.github.com>
2022-04-02 11:43:05 -0300

    oops

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const fetchAdvice = async () => {
     const res = await fetch("https://allugofrases.herokuapp.com/frases/random")
     const data = await res.json()
 
-    document.querySelector(".number").innerHTML = `ADVICE #${data.slip.id}`
-    document.querySelector(".advice").innerHTML = `"${data.slip.advice}"`
+    document.querySelector(".number").innerHTML = `ADVICE #${data.autor}`
+    document.querySelector(".advice").innerHTML = `"${data.frase}"`
 }
 
 fetchAdvice()
